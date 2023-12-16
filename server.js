bab6a029443d23fa627cbdf3e31aec2e13323050
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-16 18:30:58 -0500

    Pass it around solved

--- server.js
+++ server.js
@@ -8,9 +8,27 @@ const port = 3000;
 //main functionalty
 const app = express();
 
+//default route setup
 app.get("/", (req, res) => {
-    res.send(`Hello there`)
+    //main message and counter link setup
+    res.send(`<h1>99 bottles of beer on the wall</h1><a href="/98">Take one down, pass it around</a>`)
 });
+
+//route for decreasing the number of bottles
+app.get("/:number_of_bottles", (req, res) => {
+    const bottles = parseInt(req.params.number_of_bottles);
+    //when done
+    if(bottles === 0) {
+        //the final message and start over logic appear
+        res.send(`<h1>N more bottles of beer left on the wall</h1><a href="/"Start Over</a>`);
+
+    }   else {
+        //counting down message and logic
+        res.send(`<h1>${bottles} Bottles of beer on the wall</h1><a href="/${bottles - 1}">Take one down, pass it around</a>`);
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Im alive on port ${port}`);
